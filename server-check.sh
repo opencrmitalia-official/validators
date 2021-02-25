b402a1a05d992c83bcbaaf257d8db482eb2c1066
Francesco Bianco <info.francescobianco@gmail.com>
2021-02-25 10:58:04 +0100

    Update server-check.sh

--- server-check.sh
+++ server-check.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-lsb_release -a 
+good() {
+  echo ">> $1: OK!"
+}
+
+fail() {
+  echo "=================================="
+  echo "FATAL ERROR with $1"
+  echo "=================================="
+}
+
+release=$(lsb_release -r | grep "Release:" | cut -d" " -f2)
+
+if [[ "$release" = "18.04" ]]; then
+  good "Release di ubuntu corretta"
+else
+  fail "Problemi con la relase di ubuntu necessaria la 18.04"
+fi
+
+if [[ "$release" = "19.04" ]]; then
+  good "Release di ubuntu corretta"
+else
+  fail "Problemi con la relase di ubuntu necessaria la 18.04"
+fi
